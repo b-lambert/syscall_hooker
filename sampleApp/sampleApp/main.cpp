82452db821668ed34ca1cc4607fd0916445077bc
blahshaw <benjaminhlambert@gmail.com>
2015-01-13 02:01:32 -0800

    tried to clean up main.cpp in sample app

--- sampleApp/sampleApp/main.cpp
+++ sampleApp/sampleApp/main.cpp
@@ -10,31 +10,15 @@
 
 int main(int argc, const char * argv[])
 {
-    
     SyscallHooker hook("sample_kext.kext/Contents/MacOs/sample_kext");
-    
-    bool success = hook.AddSysCallHook("_setuid", "_setuid_hook");
-    if (success == true)
+    if (hook.AddSysCallHook("_setuid", "_setuid_hook") && hook.AddSysCallHook("_open", "_open_hook")) 
     {
-        success = hook.AddSysCallHook("_open", "_open_hook");
-        if (success == true)
-        {
-            success = hook.HookEmUp();
-            
-            std::cout << "successfully hooked setuid and open!" << std::endl;
-            
-            // hook.RestoreSysCall("_open");
-        }
-        else
-        {
-            std::cout << "Something went wrong :(" << std::endl;
-        }
+        hook.HookEmUp();
+        std::cout << "successfully hooked setuid and open!" << std::endl;
     }
     else
     {
         std::cout << "Something went wrong :(" << std::endl;
     }
-
-    
     return 0;
 }
